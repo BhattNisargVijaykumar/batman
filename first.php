a7595fbad924341bdef9844743ea74e72a1a7b0b
Nisarg Bhatt <bhattnisargv@gmail.com>
2022-01-04 14:30:37 +0530

    second output

---
+++ first.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Learn Trainee</title>
+</head>
+<body>
+
+<label for="username">Username:-</label>
+<input type="text" name="username">
+<label for="password">Password:-</label>
+<input type="Password" name="password">
+</body>
+</html>
